9278a4778aedf4cc7d58147c81af2966b7321456
rafatieppo <rafaeltieppo@yahoo.com.br>
2022-05-11 07:22:16 -0400

    install utils

---
+++ debian_run_after_install_c_utils.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# for Debian (Ubuntu Mate Xfce) - python stuff
+# new version 2022-02-13, by @rafatieppo
+
+echo -------------------------------------------------------------------
+echo Install ImageMagick ? [ 1/0 ]
+echo -------------------------------------------------------------------
+read opcao
+if [ $opcao -eq 1 ] ; then
+    apt install imagemagick
+fi
+
+echo ------------------------------------------------------------
+echo ends here.
