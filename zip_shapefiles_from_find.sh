1f9a1845650fac539bbd8ccd806d5981fe11b2ab
rafatieppo <rafaeltieppo@yahoo.com.br>
2023-08-18 14:04:05 -0400

    add condition to check if there is one shape file atleast

--- zip_shapefiles_from_find.sh
+++ zip_shapefiles_from_find.sh
@@ -10,28 +10,29 @@
 # Tags: shell, linux, regex, zip
 # ------------------------------------------------------------
 
-# step 01 - list all TCX or tcx files
-# find ./ | egrep  ".*\.(tcx|TCX)" > list_tcx_files
+# step 01 - list all shp files
 lsfiles=$(find . | egrep  ".*\.shp")
 lsshp=($lsfiles)
 
-for shpfile in "${lsshp[@]}"; do
-    # list all files for ONE shape
-    echo "working on shapefile geometry $shpfile"
-    echo "------------------------------------------------------------"
-    # echo "file name without extension"
-    # removing "./"
-    fn=$(sed -E "s/\.\///" <<< $shpfile);
-    # removing .shp
-    fn_noext=$(echo $fn | sed -e 's/.shp//');
-    # list all files for geometry
-    lsfiles_shp=$(ls ${fn_noext}.*);
-    echo "$lsfiles_shp"
-    ls ${fn_noext}.* | zip -@ "${fn_noext}.zip"
-    echo "${fn_noext}.zip" "has been writed"
-    #echo "" $fn_noext
-    echo "------------------------------------------------------------"
+if [ -z "${lsshp}"]; then
+    echo -e "---> Shape file was not found.\n"
+else
+    for shpfile in "${lsshp[@]}"; do
+        # list all files for ONE shape
+        echo "working on shapefile geometry $shpfile"
+        echo "------------------------------------------------------------"
+        # removing "./"
+        fn=$(sed -E "s/\.\///" <<< $shpfile);
+        # removing .shp
+        fn_noext=$(echo $fn | sed -e 's/.shp//');
+        # list all files for geometry
+        lsfiles_shp=$(ls ${fn_noext}.*);
+        echo "$lsfiles_shp"
+        ls ${fn_noext}.* | zip -@ "${fn_noext}.zip"
+        echo "${fn_noext}.zip" "has been writed"
+        #echo "" $fn_noext
+        echo "------------------------------------------------------------"
     done
-               
-
 echo "Its Done."
+fi
+
